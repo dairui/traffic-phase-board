4ee928bae0f6b957e73d73eb8add6908576600b3
dairui <dairui1081@163.com>
2014-08-25 21:21:53 +0800

    Removed unnecessary files and re-organized file path and naming.

--- RTX_based/Proj/obj/rtx_can.d
+++
@@ -1,4 +0,0 @@
-.\obj\rtx_can.o: ..\CAN_Core\RTX_CAN.c
-.\obj\rtx_can.o: C:\Keil\ARM\RV31\INC\RTL.h
-.\obj\rtx_can.o: C:\Keil\ARM\RV31\INC\RTX_CAN.h
-.\obj\rtx_can.o: ..\CAN_Core\CAN_Cfg.h
--- RTX_based/Proj/obj/rtx_config.d
+++
@@ -1,4 +0,0 @@
-.\obj\rtx_config.o: ..\RTXConfig\RTX_Config.c
-.\obj\rtx_config.o: C:\Keil\ARM\RV31\INC\RTL.h
-.\obj\rtx_config.o: ..\User\user_header.h
-.\obj\rtx_config.o: C:\Keil\ARM\RV31\INC\RTX_lib.c
